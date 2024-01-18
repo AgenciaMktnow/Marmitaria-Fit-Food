2b7f416ad8e1c9ad9685f9fbccb11165cc6168ae
AgenciaMktnow <151872636+AgenciaMktnow@users.noreply.github.com>
2024-01-18 12:01:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+// window.addEventListener('load', function () {
     function addWhatsapp(){
     const whatsapp=` <li class="header-actions__item account"><a href="https://api.whatsapp.com/send?phone=11989072112" target="_blank">
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="38" height="38" viewBox="0,0,256,256">
@@ -20,4 +20,4 @@ function addEndereco() {
     addWhatsapp();
     addEndereco();
 
-}); // fechar addEvent
+// }); // fechar addEvent
